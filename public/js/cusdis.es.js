829dbf06f3917b74b3f0a093919ba45df7247cf6
Modus_TheoScope <147959159+theoscope@users.noreply.github.com>
2026-05-02 23:07:05 +0800

    Fix comment css

--- public/js/cusdis.es.js
+++ public/js/cusdis.es.js
@@ -2,7 +2,7 @@ window.CUSDIS = {};
 const makeIframeContent = (target) => {
     const host = target.dataset.host || "https://cusdis.com";
     const iframeJsPath = target.dataset.iframe || `${host}/js/iframe.umd.js`;
-    const cssPath = `/src/styles/cusdis.css`;
+    const cssPath = `${host}/js/style.css`;
     return `<!DOCTYPE html>
 <html>
   <head>
